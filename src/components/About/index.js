12308c0959305a412aa00e57251617b4076c9492
Davsav <davidbyu6@gmail.com>
2021-08-01 09:09:03 -0600

    completed about styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,10 +9,12 @@ function About() {
             <div className="aboutSec">
                 <h1 id="about">About Me</h1>
                 <div className="aboutSec flex-row container">
-                    <p className="col">
+                    <p id="aboutText"className="col">
                     I am a Full Stack Developer with experience in User Experience, UX Design, Customer Journey Mapping, and focusing on the customer to find their needs. I attended the BYU Marriott School of Business where I received my BS in Experience and Design Management. I recently received a certificate in Full Stack Web Development from the University of Utah. I believe that with my unique experience and skills, I can provide unique insight into customers needs and provide them with excellent solutions to resolve their needs.
                     </p>
+                    <div className="proPicCon">
                     <img src={profilePic} id="profilePic" alt="Business Pic of David McDougal" className="col"/>
+                    </div>
                  
                 </div>
             </div>
