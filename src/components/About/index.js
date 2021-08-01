d21ac76d4c3c53a48b6dc315777d41dd6b261ab4
Davsav <davidbyu6@gmail.com>
2021-08-01 15:42:48 -0600

    more about section styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,9 +8,11 @@ function About() {
             <div className="aboutSec">
                 <h1 id="about">About Me</h1>
                 <div className="aboutSec flex-row1 container">
+                    <div className="aboutTextCont">
                     <p id="aboutText"className="col">
                     I am a Full Stack Developer with experience in User Experience, UX Design, Customer Journey Mapping, and focusing on the customer to find their needs. I attended the BYU Marriott School of Business where I received my BS in Experience and Design Management. I recently received a certificate in Full Stack Web Development from the University of Utah. I believe that with my unique experience and skills, I can provide unique insight into customers needs and provide them with excellent solutions to resolve their needs.
                     </p>
+                    </div>
                     <div className="proPicCon">
                     <img src={profilePic} id="profilePic" alt="Business Pic of David McDougal" className="col"/>
                     </div>
