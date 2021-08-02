02c0b9d6d5c04c0347892fdbeffb6e239fb359a9
Davsav <davidbyu6@gmail.com>
2021-08-01 18:38:31 -0600

    added icon

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
                 <div className="aboutSec flex-row1 container">
                     <div className="aboutTextCont">
                     <p id="aboutText"className="col">
-                    I am a Full Stack Developer with experience in User Experience, UX Design, Customer Journey Mapping, and focusing on the customer to find their needs. I attended the BYU Marriott School of Business where I received my BS in Experience and Design Management. I recently received a certificate in Full Stack Web Development from the University of Utah. I believe that with my unique experience and skills, I can provide unique insight into customers needs and provide them with excellent solutions to resolve their needs.
+                    I am a Full Stack Developer with experience in User Experience, UX Design, Customer Journey Mapping, and focusing on the customer to find their needs. I attended the BYU Marriott School of Business where I received my BS in Experience and Design Management. I recently received a certificate in Full Stack Web Development from the University of Utah. I believe that with my unique experience and skills, I can provide unique insights into customer's needs that will lead to better products and better experiences for customers.
                     </p>
                     </div>
                     <div className="proPicCon">
